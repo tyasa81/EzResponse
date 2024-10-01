5610d149f8222aea39fc9d77184783672ae4d137
Tony Yasa <tyasa81@gmail.com>
2024-10-01 20:24:33 +0700

    fix namespace of BaseRequest

--- src/Requests/BaseRequest.php
+++ src/Requests/BaseRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace tyasa81\EzCrmClient\Requests;
+namespace tyasa81\EzResponse\Requests;
 
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
